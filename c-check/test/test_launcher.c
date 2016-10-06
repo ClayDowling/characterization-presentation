efd76bf0f202767bd0e806257fd611ba1391f85c
Clay Dowling <clay@lazarusid.com>
2016-10-05 20:29:57 -0400

    Make check test suite work

--- c-check/test/test_launcher.c
+++ c-check/test/test_launcher.c
@@ -1,11 +1,11 @@
-#include "../unit.h"
+#include "../launch_program.h"
 
 #include <check.h>
 
 
-START_TEST(the_answer_to_life_the_universe_and_everything)
+START_TEST(stub_test)
 {
-    ck_assert_int_eq(answer(), 42);
+    ck_assert_int_eq(0, 42);
 }
 END_TEST
 
@@ -14,7 +14,7 @@ TCase *tcase_life(void)
 	TCase *tc;
 
 	tc = tcase_create("life");
-	tcase_add_test(tc, the_answer_to_life_the_universe_and_everything);
+	tcase_add_test(tc, stub_test);
 
 	return tc;
 }
